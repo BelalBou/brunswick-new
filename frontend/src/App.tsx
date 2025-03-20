a8e797976412e2001c183aa50256206ac51d4e26
BelalBou <belalbou48@gmail.com>
2025-03-20 22:58:44 +0100

    ajout du point d'entréé + routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,72 @@
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Container, Typography, Paper, Button, AppBar, Toolbar } from '@mui/material';
-import theme from './theme';
+import { Provider } from "react-redux";
+import store from "./store/store";
+
+// On importe le composant MyRoutes (Routes personnalisées)
+import MyRoutes from "./Routes";
+
+// On importe les actions "login", "user", "cart", "page" depuis vos slices
+import { setLoginSuccess } from "./store/slices/loginSlice";
+import {
+  setUserId,
+  setUserFirstName,
+  setUserLastName,
+  setUserLanguage,
+  setUserType,
+  setUserSupplierId,
+  setUserEmailAddress,
+  setUserPassword,
+  setUserToken
+} from "./store/slices/userSlice";
+import { setCartList } from "./store/slices/cartSlice";
+import { setSelected } from "./store/slices/pageSlice";
+
+import "./css/index.css";
 
 function App() {
+  // 1) Hydratation "user" depuis localStorage
+  const user = localStorage.getItem("user");
+  if (user) {
+    const {
+      id,
+      firstName,
+      lastName,
+      language,
+      type,
+      supplierId,
+      emailAddress,
+      password,
+      token
+    } = JSON.parse(user);
+
+    store.dispatch(setLoginSuccess(true));
+    store.dispatch(setUserId(id));
+    store.dispatch(setUserFirstName(firstName));
+    store.dispatch(setUserLastName(lastName));
+    store.dispatch(setUserLanguage(language));
+    store.dispatch(setUserType(type));
+    store.dispatch(setUserSupplierId(supplierId));
+    store.dispatch(setUserEmailAddress(emailAddress));
+    store.dispatch(setUserPassword(password));
+    store.dispatch(setUserToken(token));
+  }
+
+  // 2) Hydratation "cartList"
+  const cartList = localStorage.getItem("cartList");
+  if (cartList) {
+    store.dispatch(setCartList(JSON.parse(cartList)));
+  }
+
+  // 3) Hydratation "selected"
+  const selected = localStorage.getItem("selected");
+  if (selected) {
+    store.dispatch(setSelected(parseInt(selected, 10)));
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Brunswick
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-        <Container maxWidth="lg" sx={{ mt: 4 }}>
-          <Paper sx={{ p: 4, textAlign: 'center' }}>
-            <Typography variant="h3" gutterBottom>
-              Bienvenue sur Brunswick
-            </Typography>
-            <Typography variant="h5" color="text.secondary" paragraph>
-              Votre plateforme de gestion de commandes
-            </Typography>
-            <Box sx={{ mt: 4 }}>
-              <Button variant="contained" size="large" sx={{ mr: 2 }}>
-                Commencer
-              </Button>
-              <Button variant="outlined" size="large">
-                En savoir plus
-              </Button>
-            </Box>
-          </Paper>
-        </Container>
-      </Box>
-    </ThemeProvider>
+    <Provider store={store}>
+      {/* On affiche MyRoutes (notre composant de routes) */}
+      <MyRoutes />
+    </Provider>
   );
 }
 
---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,76 @@
+// ./Routes.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Vos imports de containers
+// import App from "./containers/AppContainer";
+// import Login from "./containers/LoginContainer";
+// import Menus from "./containers/MenusContainer";
+// import Cart from "./containers/CartContainer";
+// import Orders from "./containers/OrdersContainer";
+// import MenusCarriedAway from "./containers/MenusCarriedAwayContainer";
+// import ManageUsers from "./containers/ManageUsersContainer";
+// import ManageSuppliers from "./containers/ManageSuppliersContainer";
+// import ManageCategories from "./containers/ManageCategoriesContainer";
+// import ManageMenuSizes from "./containers/ManageMenuSizesContainer";
+// import ManageAllergies from "./containers/ManageAllergiesContainer";
+// import ManageExtras from "./containers/ManageExtrasContainer";
+// import ManageMenus from "./containers/ManageMenusContainer";
+// import ManageSettings from "./containers/ManageSettingsContainer";
+// import Account from "./containers/AccountContainer";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#002856",
+    },
+  },
+});
+
+function MyRoutes() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          {/* Page d'accueil */}
+          <Route path="/" element={<App />} />
+
+          {/* Login (avec paramètre id éventuel : "/login/:id" */}
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/login/:id" element={<Login />} /> */}
+
+          {/* Menus */}
+          {/* <Route path="/menus" element={<Menus />} />
+          <Route path="/menus/:id" element={<Menus />} /> */}
+
+          {/* Cart */}
+          {/* <Route path="/cart" element={<Cart />} />
+          <Route path="/cart/:id" element={<Cart />} /> */}
+
+          {/* Orders */}
+          {/* <Route path="/orders" element={<Orders />} />
+          <Route path="/orders/:id" element={<Orders />} />
+          <Route path="/menus-carried-away" element={<MenusCarriedAway />} /> */}
+
+          {/* Account */}
+          {/* <Route path="/account" element={<Account />} /> */}
+
+          {/* Gestion */}
+          {/* <Route path="/manage-users" element={<ManageUsers />} />
+          <Route path="/manage-suppliers" element={<ManageSuppliers />} />
+          <Route path="/manage-categories" element={<ManageCategories />} />
+          <Route path="/manage-menu-sizes" element={<ManageMenuSizes />} />
+          <Route path="/manage-allergies" element={<ManageAllergies />} />
+          <Route path="/manage-extras" element={<ManageExtras />} />
+          <Route path="/manage-menus" element={<ManageMenus />} />
+          <Route path="/manage-settings" element={<ManageSettings />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default MyRoutes;
