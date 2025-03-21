811bc635bc45a25173aa40d5bb4e770f8bad2ef2
BelalBou <belalbou48@gmail.com>
2025-03-21 13:24:11 +0100

    a

--- frontend/src/components/MenusList/MenusList.tsx
+++ frontend/src/components/MenusList/MenusList.tsx
@@ -1,12 +1,13 @@
 // ./components/MenuCard/MenusList.tsx
 import { Fragment } from "react";
 import {
-  List,
-  ListItem,
-  ListItemText,
-  Divider,
-  ListItemIcon,
+  Grid,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
   Typography,
+  Box
 } from "@mui/material";
 import MenusListItem from "../MenusListItem/MenusListItem";
 import placeHolderIcon from "../../images/placeholder.svg";
@@ -29,110 +30,78 @@ export default function MenusList({
     menu.menu_size_id ? menu.menu_size_id === menuList[0].menu_size_id : true
   );
 
-  const sxList = {
-    width: "100%",
-    "@media (minWidth: 600px)": {
-      width: "100%",
-    },
-  };
-
-  // const sxListItem = {
-  //   "@media (minWidth: 600px)": {
-  //     padding: "16px",
-  //   },
-  // };
-
-  // const sxListItemText = {
-  //   "@media (minWidth: 600px)": {
-  //     marginRight: "16px",
-  //   },
-  // };
-
   return (
-    <List sx={sxList}>
-      {filteredMenuList.map((menu) => (
-        <Fragment key={menu.id}>
-          {menuList.filter((x) => x.title === menu.title).length > 1 ? (
-            <MenusListItem
-              menu={menu}
-              userLanguage={userLanguage}
-              menuList={menuList}
-              onOpenAdd={onOpenAdd}
-            />
-          ) : (
-            <>
-              <ListItem component="button" onClick={() => onOpenAdd(menu)}>
-                <ListItemText
-                  primary={
-                    <Typography variant="subtitle1" noWrap>
-                      {menu.title}
-                    </Typography>
-                  }
-                  secondary={
-                    <>
+    <Box sx={{ flexGrow: 1, padding: 2 }}>
+      <Grid container spacing={2}>
+        {filteredMenuList.map((menu) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={menu.id}>
+            {menuList.filter((x) => x.title === menu.title).length > 1 ? (
+              <MenusListItem
+                menu={menu}
+                userLanguage={userLanguage}
+                menuList={menuList}
+                onOpenAdd={onOpenAdd}
+              />
+            ) : (
+              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                <CardActionArea onClick={() => onOpenAdd(menu)} sx={{ height: '100%' }}>
+                  <Box sx={{ display: 'flex', padding: 2 }}>
+                    <Box sx={{ flex: '1 1 auto' }}>
+                      <Typography variant="subtitle1" component="div" gutterBottom noWrap>
+                        {menu.title}
+                      </Typography>
+                      
                       {menu.MenuSize && (
                         <Typography
-                          component="span"
                           variant="body2"
                           color="text.secondary"
-                          sx={{ mt: 1, display: 'inline-block', width: '100%' }}
+                          sx={{ mt: 1 }}
                         >
                           {userLanguage === "en"
                             ? menu.MenuSize.title_en
                             : menu.MenuSize.title}
                         </Typography>
                       )}
+                      
                       <Typography
-                        component="span"
                         variant="body2"
                         color="text.secondary"
-                        sx={{ mt: 1, display: 'inline-block', width: '100%' }}
+                        sx={{ mt: 1 }}
                         noWrap
                       >
-                        {menu.description ? (
-                          <span>
-                            {menu.description}
-                            <br />
-                          </span>
-                        ) : (
-                          ""
-                        )}
+                        {menu.description || ""}
                       </Typography>
+                      
                       <Typography
-                        component="span"
                         variant="subtitle1"
                         color="primary"
-                        sx={{ fontWeight: 550, mt: 1, display: 'inline-block', width: '100%' }}
+                        sx={{ fontWeight: 550, mt: 1 }}
                       >
                         {parseFloat(menu.pricing).toLocaleString("fr", {
                           minimumFractionDigits: 2,
                         })}{" "}
                         €
                       </Typography>
-                    </>
-                  }
-                />
-                <ListItemIcon>
-                  <img
-                    src={
-                      menu.picture
-                        ? `${S3_BASE_URL}/${menu.picture}`
-                        : placeHolderIcon
-                    }
-                    alt="menu"
-                    style={{
-                      width: "96px",
-                      height: "96px",
-                      borderRadius: "3px",
-                    }}
-                  />
-                </ListItemIcon>
-              </ListItem>
-              <Divider />
-            </>
-          )}
-        </Fragment>
-      ))}
-    </List>
+                    </Box>
+                    
+                    <CardMedia
+                      component="img"
+                      sx={{ 
+                        width: 96, 
+                        height: 96, 
+                        borderRadius: 1,
+                        objectFit: 'cover'
+                      }}
+                      image={menu.picture ? `${S3_BASE_URL}/${menu.picture}` : placeHolderIcon}
+                      alt={menu.title}
+                    />
+                  </Box>
+                </CardActionArea>
+              </Card>
+            )}
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
